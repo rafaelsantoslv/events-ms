0a9cacc78074c1e10593ef381e8b1896cc1898ef
Rafael dos Santos Batista <rafaeldsantos442@gmail.com>
2025-03-20 18:22:35 -0300

    feat: Realizado criação de entidade de evento

---
+++ src/main/java/com/rafael/ms_events/entity/EventEntity.java
@@ -0,0 +1,23 @@
+package com.rafael.ms_events.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name="event")
+@Table(name="event")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of="id")
+public class EventEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private int maxParticipants;
+    private int registeredParticipants;
+    private String data;
+    private String title;
+    private String description;
+
+}

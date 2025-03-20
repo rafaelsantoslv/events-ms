3611ffe8fb01e14ce862236212e4352456c27525
Rafael dos Santos Batista <rafaeldsantos442@gmail.com>
2025-03-20 17:22:20 -0300

    first commit

---
+++ src/main/java/com/rafael/ms_events/MsEventsApplication.java
@@ -0,0 +1,13 @@
+package com.rafael.ms_events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafael/ms_events/MsEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.ms_events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
